afbefbf4a99e5af36405c499ca90077cea1cfb40
[SW2/서울] 김정현 <6539305@gmail.com>
2022-07-04 22:05:06 +0900

    chore: Dev 포트 변경 3000 -> 3030

--- front-team7/webpack/webpack.dev.js
+++ front-team7/webpack/webpack.dev.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'development',
   devtool: 'eval-source-map',
   devServer: {
-    port: 3000, // 요청을 수신할 포트 번호를 지정하세요.
+    port: 3030, // 요청을 수신할 포트 번호를 지정하세요.
     hot: true, // Webpack의 Hot Module Replacement 기능을 활성화합니다.
     open: true, // 서버가 시작된 후 브라우저를 열도록 dev-server에 지시합니다.
     compress: true, // 제공되는 모든 항목에 대해 gzip 압축을 활성화합니다.
