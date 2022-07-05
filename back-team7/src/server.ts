5563c934f403a72ce3509d7ca546ac6c17b7a99b
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-07-05 17:02:55 +0900

    feat : google 로그인 추가 기존 작업했던 코드 이용

--- back-team7/src/server.ts
+++ back-team7/src/server.ts
@@ -1,17 +1,17 @@
 import express from 'express';
-// import cors from 'cors';
-// import passport from 'passport';
-// import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import passport from 'passport';
+import cookieParser from 'cookie-parser';
 import 'dotenv/config';
-// import { apiRouter, authRouter } from './routes';
-// import { errorHandler, getUserFromJWT } from './middlewares';
-// import { usePassport } from './passport';
+import { apiRouter, authRouter } from './routes';
+import { errorHandler, getUserFromJWT } from './middlewares';
+import { usePassport } from './passport';
 // import webSocket from './socket';
 // import path from 'path';
 
-// usePassport();
+usePassport();
 const app = express();
-// app.use(cors());
+app.use(cors());
 
 // Content-Type: application/json 형태의 데이터를 인식하고 핸들링할 수 있게 함.
 app.use(express.json());
@@ -19,18 +19,18 @@ app.use(express.json());
 // Content-Type: application/x-www-form-urlencoded 형태의 데이터를 인식하고 핸들링할 수 있게 함.
 app.use(express.urlencoded({ extended: false }));
 
-// app.use(cookieParser());
+app.use(cookieParser());
 
 const PORT = process.env.PORT;
 
-// app.use(passport.initialize());
+app.use(passport.initialize());
 
-// app.use(getUserFromJWT);
+app.use(getUserFromJWT);
 
-// app.use('/api', apiRouter);
-// app.use('/auth', authRouter);
+app.use('/api', apiRouter);
+app.use('/auth', authRouter);
 
-// app.use(errorHandler);
+app.use(errorHandler);
 
 const server = app.listen(PORT, () => console.log(`server is running ${PORT}`));
 
