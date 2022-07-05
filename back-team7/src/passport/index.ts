b5e2dd0528ece5e58aab6817b519f6a6a357c3db
조재홍 <jojaehong@jojaehong-ui-MacBookAir.local>
2022-07-05 22:10:11 +0900

    feat:kakao-oauth

--- back-team7/src/passport/index.ts
+++ back-team7/src/passport/index.ts
@@ -2,8 +2,10 @@ import passport from 'passport';
 
 import { jwt } from './strategies/jwt';
 import { google } from './strategies/google';
+const kakao = require('./strategies/kakao');
 
 export function usePassport() {
   passport.use(jwt);
   passport.use(google);
+  passport.use(kakao);
 }
