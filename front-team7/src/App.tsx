d23bf9d9d49ce8fdedf71f7b382d10864dcd1c45
dnjsqhwo <dnjsqhwo@gmail.com>
2022-07-05 21:47:06 +0900

    feat: 리액트 라우팅 기능 추가

--- front-team7/src/App.tsx
+++ front-team7/src/App.tsx
@@ -1,44 +1,15 @@
-import { GlobalStyle } from '@/commons/design-system/global-style';
-import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Hi from '@/commons/Hi';
+import Home from '@/pages/Home';
 
 const App = () => {
-  const [hi, setHi] = useState('how!');
-
-  const apiFetchData = async () => {
-    const res = await fetch('/api/data', {
-      method: 'GET',
-    });
-
-    const data = (await res.json()) as { message: string };
-    console.log(data);
-
-    return data;
-  };
-
-  const authFetchData = async () => {
-    const res = await fetch('/auth/data', {
-      method: 'GET',
-    });
-
-    const data = (await res.json()) as { message: string };
-    console.log(data);
-
-    return data;
-  };
-
-  useEffect(() => {
-    apiFetchData().catch((e) => console.log(e));
-    authFetchData().catch((e) => console.log(e));
-  }, []);
-
   return (
     <>
-      <GlobalStyle />
-      <Hi hi={hi} />
-      <Hi hi={hi} />
-      <Hi hi={'asdasdas'} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
