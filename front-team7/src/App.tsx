c0a58aa4af096eb1bc19bbc0497ee04ac20e51e4
shinbian11 <shinbian11@gmail.com>
2022-07-06 15:49:05 +0900

    chore : test용 코드 정리

--- front-team7/src/App.tsx
+++ front-team7/src/App.tsx
@@ -6,8 +6,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { defaultTheme, GlobalStyle } from '@/style';
 
 import Home from '@/pages/Home';
-//  나중에 test하고 지우고 push 할지 이야기해보기
-import FeedDetail from '@/components/Layout/FeedDetail/FeedDetail';
 
 const App = () => {
   return (
@@ -17,8 +15,6 @@ const App = () => {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* 나중에 test하고 지우고 push 할지 이야기해보기 */}
-            <Route path="/feed-detail" element={<FeedDetail />} />
           </Routes>
         </BrowserRouter>
       </ThemeProvider>
