9820bd1ed079507f1e5296dfa400e2c473288284
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-07-06 23:42:25 +0900

    feat : 관리자 확인 기능 추가 작업 중 type error 발생 관련 코드 주석 처리

--- back-team7/src/routes/api.ts
+++ back-team7/src/routes/api.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { userRouter } from './api/';
-// import { loginRequired } from '../middlewares';
+import { loginRequired } from '../middlewares';
 
 const apiRouter = Router();
 
-apiRouter.use('/users', /*loginRequired,*/ userRouter);
+apiRouter.use('/users', userRouter);
+// apiRouter.use('/users', loginRequired, userRouter);
 
 export { apiRouter };
