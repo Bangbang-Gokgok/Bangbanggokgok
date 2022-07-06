fa1ad0c5f99aa518ae5e6f03a0b33ea7ce414eb4
dnjsqhwo <dnjsqhwo@gmail.com>
2022-07-06 16:36:02 +0900

    Home 임시 변경

--- front-team7/src/pages/Home.tsx
+++ front-team7/src/pages/Home.tsx
@@ -1,5 +1,42 @@
+import bill from '@/assets/images/bill.jpg';
+
+import { Main } from '@/components/Layout';
+import { Avartar } from '@/components/Avatar';
+import { UserInfo } from '@/components/UserInfo';
+import { FeedFolded } from '@/components/FeedFolded';
+import { flex } from 'styled-system';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Main>
+      <div style={{ display: 'flex', gap: '10px', marginBottom: '20px' }}>
+        <Avartar size="sm" />
+        <Avartar size="md" />
+        <Avartar kind="square" size="lg" />
+      </div>
+
+      <div style={{ display: 'flex', gap: '10px', marginBottom: '20px' }}>
+        <Avartar kind="circle" size="sm" src={bill as string} />
+        <Avartar kind="circle" size="md" src={bill as string} />
+        <Avartar kind="circle" size="lg" src={bill as string} />
+      </div>
+
+      <div style={{ display: 'flex', gap: '10px', marginBottom: '20px' }}>
+        <UserInfo name="Bill Gates" image={bill as string} />
+        <UserInfo name="unknown" />
+      </div>
+
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', marginBottom: '20px' }}>
+        <FeedFolded name="Bill Gates" image={bill as string} title="가평에서 보낸 하룻밤 😋" />
+        <FeedFolded
+          name="Bill Gates"
+          image={bill as string}
+          title="내가 마소에 있을 때 말이지... 😎"
+        />
+        <FeedFolded name="xxxx" title="맛집을 찾았다! " />
+      </div>
+    </Main>
+  );
 };
 
 export default Home;
