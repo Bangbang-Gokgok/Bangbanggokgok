3963760e0ceb7d40a298cf94711b00b5a67d2bb2
dnjsqhwo <dnjsqhwo@gmail.com>
2022-07-06 16:33:11 +0900

    feat: 아바타 컴포넌트 구현

---
+++ front-team7/src/components/Avatar/Avartar.variant.tsx
@@ -0,0 +1,43 @@
+export const getKindVariant = () => {
+  const kindVariant = {
+    prop: 'kind',
+    variants: {
+      circle: {
+        borderRadius: '50%',
+      },
+      square: {
+        borderRadius: '5px',
+      },
+    },
+  };
+
+  return kindVariant;
+};
+
+export const getSizeVariant = () => {
+  const sizeVariant = {
+    prop: 'size',
+    variants: {
+      sm: {
+        width: '24px',
+        height: '24px',
+        minWidth: '24px',
+        minHeight: '24px',
+      },
+      md: {
+        width: '32px',
+        height: '32px',
+        minWidth: '32px',
+        minHeight: '32px',
+      },
+      lg: {
+        width: '40px',
+        height: '40px',
+        minWidth: '40px',
+        minHeight: '40px',
+      },
+    },
+  };
+
+  return sizeVariant;
+};
