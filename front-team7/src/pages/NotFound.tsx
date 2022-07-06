c9d50ced493fecfa30e04868595809a023629b48
[SW2/서울] 김정현 <6539305@gmail.com>
2022-07-07 00:22:24 +0900

    style: NotFound 페이지 스타일 수정

--- front-team7/src/pages/NotFound.tsx
+++ front-team7/src/pages/NotFound.tsx
@@ -1,23 +1,106 @@
+import { Main } from "@/components/Layout";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { AiOutlineHome, AiFillFrown } from 'react-icons/ai';
+import { TbArrowBackUp } from 'react-icons/tb';
 
 const NotFound = () => {
   return (
-    <Container>
-      <Comment>404 not found</Comment>
-    </Container>
+    <Main>
+      <Title>
+        <CommentIcon><AiFillFrown /></CommentIcon>
+        <CommentTitle>404</CommentTitle>
+        <Comment>NOT FOUND</Comment>
+        <GoHome><LinkTag to="/">Go Home</LinkTag></GoHome>
+      </Title>
+
+    </Main>
   );
 };
 
-const Container = styled.div`
+const Title = styled.p`
+  width: 100%;
+  height: 500px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  font-weight: 300;
+  font-size: 7rem;
+  color: black;
+  p{
+    text-align: center;
+  }
 `;
 
-const Comment = styled.span`
-  font-size: 2rem;
-  color: blue;
+const CommentTitle = styled.span`
+  width: 100%;
+  text-align: center;
+  line-height: 6.5rem;
+  color: ${(props) => props.theme.accentColor};
+  -webkit-clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 80%);
+  clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 80%);
+  transform: translateY(-50px);
+  opacity: 0;
+  animation-name: titleAnimation;
+  animation-timing-function:ease;
+  animation-duration: 5s;
+  animation-delay: 0.6s;
+  animation-iteration-count: infinite;
+  &:first-child{
+    animation-delay: 0.7s;
+  }
+  &:last-child{
+    animation-delay: 0.5s;
+  }
+
+  @keyframes titleAnimation {
+    0% {
+      transform: translateY(-50px);
+      opacity: 0;
+      -webkit-clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 80%);
+      clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 80%);
+    }
+    20% {
+        transform: translateY(0);
+        opacity: 1;
+        -webkit-clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 15%);
+        clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 15%);
+    }
+    80% {
+        transform: translateY(0);
+        opacity: 1;
+        -webkit-clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 15%);
+        clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 15%);
+    }
+    100% {
+        transform: translateY(50px);
+        opacity: 0;
+        -webkit-clip-path: polygon(100% 0, 100% -0%, 0 100%, 0 100%);
+        clip-path: polygon(100% 0, 100% -0%, 0 100%, 0 100%);
+    }
+  }
+`;
+
+const Comment = styled(CommentTitle)`
+  font-size: 3rem;
+`;
+
+const CommentIcon = styled(CommentTitle)`
+  font-size: 10rem;
+`;
+
+const GoHome = styled.button`
+    border: 1px solid #f39c12;
+    border-radius: 5px;
+    background-color: #f39c12;
+    padding: 10px;
+    font-size: 2.5rem;
+    color: black;
+`;
+
+const LinkTag = styled(Link)`
+  &:visited { text-decoration: none; }
 `;
 
 export default NotFound;
