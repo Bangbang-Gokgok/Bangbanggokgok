c51d37fc5764155c86ae2b6fea4f434ac1f00f54
shinbian11 <shinbian11@gmail.com>
2022-07-11 16:14:34 +0900

    fix : Feed - axios 호출 => try ~ catch 예외 처리 기본 코드 추가

--- front-team7/src/api/feeds.ts
+++ front-team7/src/api/feeds.ts
@@ -1,37 +1,55 @@
 import axios from 'axios';
 
-// 추후에 try~catch 문 이용하여 예외 처리 하기
-
 export const getAllFeeds = async () => {
-  let res = await axios.get('/api/feeds/list');
-  // console.log('res.data : ', res.data);
-  return res.data;
+  try {
+    let res = await axios.get('/api/feeds/list');
+    // console.log('res.data : ', res.data);
+    return res.data;
+  } catch (err) {
+    return err;
+  }
 };
 
 export const getOneFeed = async (id) => {
-  let res = await axios.get(`/api/feeds/${id}`);
-  // console.log('res.data : ', res.data);
-  return res.data;
+  try {
+    let res = await axios.get(`/api/feeds/${id}`);
+    // console.log('res.data : ', res.data);
+    return res.data;
+  } catch (err) {
+    return err;
+  }
 };
 
 export const createOneFeed = async (sendData) => {
-  let res = await axios.post(`/api/feeds`, JSON.stringify(sendData), {
-    headers: { 'Content-Type': `application/json` },
-  });
-  // console.log('res.data : ', res.data);
-  return res.data;
+  try {
+    let res = await axios.post(`/api/feeds`, JSON.stringify(sendData), {
+      headers: { 'Content-Type': `application/json` },
+    });
+    // console.log('res.data : ', res.data);
+    return res.data;
+  } catch (err) {
+    return err;
+  }
 };
 
 export const updateOneFeed = async (id, sendData) => {
-  let res = await axios.put(`/api/feeds/${id}`, JSON.stringify(sendData), {
-    headers: { 'Content-Type': `application/json` },
-  });
-  console.log('updated res.data : ', res.data);
-  return res.data;
+  try {
+    let res = await axios.put(`/api/feeds/${id}`, JSON.stringify(sendData), {
+      headers: { 'Content-Type': `application/json` },
+    });
+    // console.log('updated res.data : ', res.data);
+    return res.data;
+  } catch (err) {
+    return err;
+  }
 };
 
 export const deleteOneFeed = async (id) => {
-  let res = await axios.delete(`/api/feeds/${id}`);
-  // console.log('deleted res.data : ', res.data);
-  return res.data;
+  try {
+    let res = await axios.delete(`/api/feeds/${id}`);
+    // console.log('deleted res.data : ', res.data);
+    return res.data;
+  } catch (err) {
+    return err;
+  }
 };
