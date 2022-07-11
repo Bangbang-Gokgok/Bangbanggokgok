53908e1b2c4e35d7255e77df3e1ee3ac0d5586f7
dnjsqhwo <dnjsqhwo@gmail.com>
2022-07-11 11:30:08 +0900

    chore: 페이지 명 변경

---
+++ front-team7/src/pages/HomePage.tsx
@@ -0,0 +1,37 @@
+import { Main } from '@/components/Layout';
+import styled from 'styled-components';
+import FeedDetail from '@/components/Layout/FeedDetail/FeedDetail';
+import unknownUser from '@/assets/images/unknown-user.png';
+const FeedListContainer = styled.div`
+  width: 330px;
+  display: flex;
+  flex-direction: column;
+  justify-contents: center;
+  align-itmes: center;
+
+  padding: 30px 0;
+  gap: 30px;
+`;
+
+const Home = () => {
+  let name = '김지환';
+  let title = '👍🏽 홀로 여행기';
+
+  return (
+    <Main
+      display={'flex'}
+      flexDirection={'column'}
+      justifyContent={'flex-start'}
+      alignItems={'center'}
+    >
+      <FeedListContainer>
+        <FeedDetail name={name} image={unknownUser as string} title={title}></FeedDetail>
+        <FeedDetail name={name} image={unknownUser as string} title={title}></FeedDetail>
+        <FeedDetail name={name} image={unknownUser as string} title={title}></FeedDetail>
+        <FeedDetail name={name} image={unknownUser as string} title={title}></FeedDetail>
+      </FeedListContainer>
+    </Main>
+  );
+};
+
+export default Home;
