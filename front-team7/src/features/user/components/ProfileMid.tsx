f35b3984df28bd7ece9ce93227c63dc8635ffbf7
dnjsqhwo <dnjsqhwo@gmail.com>
2022-07-12 01:06:04 +0900

    feat: 프로필 페이지 UI 개발

--- front-team7/src/features/user/components/ProfileMid.tsx
+++ front-team7/src/features/user/components/ProfileMid.tsx
@@ -1,3 +1,34 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
 export const ProfileMid = () => {
-  return <div>ProfileMid</div>;
+  return (
+    <StyledProfileMid>
+      <span className="introduction">나의 지도를 소개합니다 ✨</span>
+      <span className="content">다채롭고 색다른, 우아하고 멋있는 홍길동의 지도입니다.</span>
+    </StyledProfileMid>
+  );
 };
+
+const StyledProfileMid = styled.div`
+  min-height: 20%;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  padding: 16px 10px;
+
+  .introduction {
+    color: whitesmoke;
+    font-size: 1.5rem;
+    align-self: center;
+  }
+
+  .content {
+    color: #333333;
+    font-size: 1.4rem;
+    background-color: #f3f6fb;
+    border-radius: 5px;
+    padding: 12px 16px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  }
+`;
--- front-team7/src/features/user/components/ProfileTop.tsx
+++ front-team7/src/features/user/components/ProfileTop.tsx
@@ -1,10 +1,131 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { AiFillSetting } from 'react-icons/ai';
+
+import profileBackground from '@/assets/images/profile-background.jpg';
+
+import { Avartar } from '@/components/Avatar';
+
+const dummyData = [
+  { title: '게시글', number: 15 },
+  { title: '친구', number: 30 },
+];
 
 export const ProfileTop = () => {
-  return <StyledProfileTop>ProfileTop</StyledProfileTop>;
+  return (
+    <StyledProfileTop image={profileBackground as string}>
+      <div className="profile-icon">
+        <StyledLinkButton to="/profile-setting">
+          <AiFillSetting />
+        </StyledLinkButton>
+      </div>
+      <div className="profile-main">
+        <Avartar size="xl" />
+        <span className="username">홍길동</span>
+        <span className="email">honggil@naver.com</span>
+        <div className="profile-info-container">
+          {dummyData.map(({ title, number }) => (
+            <div className="profile-info" key={title}>
+              <span className="profile-info-title">{title}</span>
+              <span className="profile-info-number">{number}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </StyledProfileTop>
+  );
 };
 
-const StyledProfileTop = styled.div`
-  height: 40%;
-  background-color: white;
+const StyledProfileTop = styled.div<{ image: string }>`
+  position: relative;
+  height: 55%;
+
+  :before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: ${({ image }) => `url(${image})`};
+    background-size: cover;
+    filter: grayscale(80%);
+    opacity: 0.55;
+  }
+
+  .profile-icon {
+    position: absolute;
+    right: 0;
+    padding: 8px;
+  }
+
+  .profile-main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: whitesmoke;
+    gap: 3px;
+
+    .username {
+      font-size: 1.75rem;
+      font-weight: bold;
+    }
+
+    .email {
+      font-size: 1.4rem;
+    }
+  }
+
+  .profile-info-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 100%;
+    font-size: 1.4rem;
+    color: black;
+    gap: 6px;
+
+    .profile-info {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      min-width: 62px;
+      padding: 6px 0px;
+      height: 100%;
+      border-radius: 5px;
+      background-color: #f3f6fb;
+      box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+
+      .profile-info-title {
+        color: #2f2f2f;
+        font-weight: bold;
+      }
+
+      .profile-info-number {
+      }
+    }
+  }
+`;
+
+const StyledLinkButton = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2.5rem;
+  color: whitesmoke;
+  height: 45px;
+  width: 45px;
+  background-color: #8d3030;
+  border-radius: 50%;
+  padding: 6px;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: gold;
+  }
 `;
