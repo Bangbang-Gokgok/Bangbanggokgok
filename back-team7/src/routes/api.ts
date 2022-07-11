46971bc80cca9a24a345ee0b4df5a81ed731bfa0
조재홍 <jojaehong@jojaehong-ui-MacBookAir.local>
2022-07-11 09:56:32 +0900

    feat : feed, review 기본 CRUD

--- back-team7/src/routes/api.ts
+++ back-team7/src/routes/api.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { userRouter } from './api/';
+import { feedRouter } from './api/';
+import { reviewRouter } from './api/';
 // import { loginRequired } from '../middlewares';
 
 const apiRouter = Router();
 
 apiRouter.use('/users', /*loginRequired,*/ userRouter);
-
+apiRouter.use('/feeds', /*feedRequired,*/ feedRouter);
+apiRouter.use('/reviews', /*reviewRequired,*/ reviewRouter);
 export { apiRouter };
