4637ae87ad54f900f461601c65445d6a3349d311
dnjsqhwo <dnjsqhwo@gmail.com>
2022-07-12 20:08:44 +0900

    chore: react-hooks/exhaustive-deps 옵션 off

--- front-team7/.eslintrc.js
+++ front-team7/.eslintrc.js
@@ -30,7 +30,7 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': [
-      'warn',
+      'off',
       {
         additionalHooks: 'useRecoilCallback',
       },
