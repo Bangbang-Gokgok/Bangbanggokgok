012be821b68079b506b78b863977dc2251af5a79
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-07-12 13:09:28 +0900

    fix : user 생성 버그 수정 - ValidationError

--- back-team7/src/services/users.ts
+++ back-team7/src/services/users.ts
@@ -5,7 +5,7 @@ export interface UserInfo {
   authority: string;
   email: string;
   name: string;
-  refreshToken: string;
+  refreshToken?: string | undefined;
   profileImage?: string | undefined;
   contactNumber?: number | undefined;
   location?: object | undefined;
@@ -38,7 +38,7 @@ class UserService {
   async getUsers(): Promise<Partial<UserData>[]> {
     const users = await User.find({});
     const data = await users.map(({ _id, name, profileImage }) => ({ _id, name, profileImage }));
-    return data;
+    return users;
   }
 
   async getUserDataById(_id: Types.ObjectId | string): Promise<UserData> {
