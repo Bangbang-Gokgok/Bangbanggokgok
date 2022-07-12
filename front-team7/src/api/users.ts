5d2f8275f042350702eeb9d27f958c943d6f1523
task11 <task11@naver.com>
2022-07-12 16:04:12 +0900

    chore: lodash 제거

---
+++ front-team7/src/api/users.ts
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+export const getMyUserInfo = async () => {
+  try {
+    let res = await axios.get('/api/users/user');
+    // console.log('res.data : ', res.data);
+    return res.data;
+  } catch (err) {
+    return err;
+  }
+};
+
+export const getAllUsers = async () => {
+  try {
+    let res = await axios.get('/api/users/list');
+    // console.log('res.data : ', res.data);
+    return res.data;
+  } catch (err) {
+    return err;
+  }
+};
+
+export const getOneUser = async (id) => {
+  try {
+    let res = await axios.get(`/api/users/${id}`);
+    // console.log('res.data : ', res.data);
+    return res.data;
+  } catch (err) {
+    return err;
+  }
+};
+
+// 본인의 아이디로 로그인을 해야 TEST 가능한 메소드 : updateUser, deleteUser
+
+export const updateUser = async (updateData) => {
+  try {
+    let res = await axios.put(`/api/users/user`, JSON.stringify(updateData), {
+      headers: { 'Content-Type': `application/json` },
+    });
+    // console.log('updated res.data : ', res.data);
+    return res.data;
+  } catch (err) {
+    return err;
+  }
+};
+
+export const deleteUser = async () => {
+  try {
+    let res = await axios.delete(`/api/users/user`);
+    // console.log('deleted res.data : ', res.data);
+    return res.data;
+  } catch (err) {
+    return err;
+  }
+};
