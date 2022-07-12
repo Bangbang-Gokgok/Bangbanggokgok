dec2be3a14cbf24fc80b42dee9336aada0710564
shinbian11 <shinbian11@gmail.com>
2022-07-12 16:10:50 +0900

    fix : 무한스크롤은 InfiniteScrollHomePage.tsx에 일단 임시저장 (/temp로 접근 가능) => css 문제 해결한 이후에 HomePage.tsx에 반영

--- front-team7/src/Router.tsx
+++ front-team7/src/Router.tsx
@@ -7,30 +7,53 @@ import LoginPage from '@/pages/LoginPage';
 import NotFoundPage from '@/pages/NotFoundPage';
 import HomePage from '@/pages/HomePage';
 import AuthRouter from '@/components/AuthRouter';
+import InfiniteScrollHomePage from './pages/InfiniteScrollHomePage';
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={
-          <AuthRouter>
-            <HomePage />
-          </AuthRouter>
-        } />
-        <Route path="/feedmap/:userId" element={
-          <AuthRouter>
-            <FeedMapPage />
-          </AuthRouter>
-        } />
-        <Route path="/search" element={
-          <AuthRouter>
-            <SearchPage />
-          </AuthRouter>} />
-        <Route path="/profile" element={
-          <AuthRouter>
-            <ProfilePage />
-          </AuthRouter>
-        } />
+        {/* /temp는 무한 스크롤 css 문제 해결용도로 잠깐 만들어놓은 것! 무한 스크롤 css 문제 해결 후 삭제할 예정입니다. */}
+        <Route
+          path="/temp"
+          element={
+            <AuthRouter>
+              <InfiniteScrollHomePage />
+            </AuthRouter>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <AuthRouter>
+              <HomePage />
+            </AuthRouter>
+          }
+        />
+        <Route
+          path="/feedmap/:userId"
+          element={
+            <AuthRouter>
+              <FeedMapPage />
+            </AuthRouter>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <AuthRouter>
+              <SearchPage />
+            </AuthRouter>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <AuthRouter>
+              <ProfilePage />
+            </AuthRouter>
+          }
+        />
         <Route path="*" element={<NotFoundPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
---
+++ front-team7/src/pages/InfiniteScrollHomePage.tsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import { Main } from '@/components/Layout';
+import styled from 'styled-components';
+import FeedDetail from '@/components/Layout/FeedDetail/FeedDetail';
+import unknownUser from '@/assets/images/unknown-user.png';
+import InfiniteScroll from 'react-infinite-scroll-component';
+// import { useEffect } from '@storybook/addons';
+
+const FeedListContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  // padding: 30px 0;
+  // gap: 30px;
+`;
+
+interface MOCK {
+  name: string;
+  title: string;
+}
+const MOCK_ITEMS: MOCK[] = [
+  { name: '김지환', title: '👍🏽 홀로 여행기1' },
+  { name: '김지환', title: '👍🏽 홀로 여행기2' },
+  { name: '김지환', title: '👍🏽 홀로 여행기3' },
+];
+
+const InfiniteScrollHomePage = () => {
+  // let name = '김지환';
+  // let title = '👍🏽 홀로 여행기';
+
+  const [items, setItems] = useState<MOCK[]>(MOCK_ITEMS);
+  const [hasMore, setHasMore] = useState<boolean>(true);
+
+  const fetchMoreData = () => {
+    if (items.length >= 9) {
+      setHasMore(false);
+      return;
+    }
+    setTimeout(() => {
+      const newItems = items.concat(MOCK_ITEMS);
+      setItems(newItems);
+    }, 1000);
+  };
+
+  return (
+    <Main
+      id="main-styled"
+      display={'flex'}
+      flexDirection={'column'}
+      justifyContent={'flex-start'}
+      alignItems={'center'}
+    >
+      <FeedListContainer>
+        <div style={{ width: '90%' }}>
+          <InfiniteScroll
+            dataLength={items.length}
+            next={fetchMoreData}
+            hasMore={hasMore}
+            endMessage={<h4>모든 데이터 로드 완료!</h4>}
+            loader={<h4>Loading...</h4>}
+            scrollableTarget="main-styled"
+          >
+            {items.map((item, index) => (
+              <FeedDetail
+                key={index}
+                name={item.name}
+                image={unknownUser as string}
+                title={item.title}
+                desc={'description'}
+              ></FeedDetail>
+            ))}
+          </InfiniteScroll>
+        </div>
+      </FeedListContainer>
+    </Main>
+  );
+};
+
+export default InfiniteScrollHomePage;
