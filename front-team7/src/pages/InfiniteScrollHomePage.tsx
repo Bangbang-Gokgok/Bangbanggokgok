ab9a6223ea7d629ca6b27e37bbbad77f0121682f
shinbian11 <shinbian11@gmail.com>
2022-07-12 20:26:46 +0900

    fix : 무한 스크롤 css 문제 해결

--- front-team7/src/pages/InfiniteScrollHomePage.tsx
+++ front-team7/src/pages/InfiniteScrollHomePage.tsx
@@ -8,7 +8,7 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 
 const FeedListContainer = styled.div`
   width: 100%;
-  height: 100vh;
+  // height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -54,30 +54,26 @@ const InfiniteScrollHomePage = () => {
       // justifyContent={'flex-start'}
       alignItems={'center'}
       padding={'70px 0'}
-      // 임의로 준 높이값. 위로 스크롤 잘 안됨 + 다른 크기의 창에서는 또 위아래가 잘려서 보임...
-      height={'800px'}
     >
       <FeedListContainer>
-        <div style={{ width: '90%' }}>
-          <InfiniteScroll
-            dataLength={items.length}
-            next={fetchMoreData}
-            hasMore={hasMore}
-            endMessage={<h4>모든 데이터 로드 완료!</h4>}
-            loader={<h4>Loading...</h4>}
-            scrollableTarget="main-styled"
-          >
-            {items.map((item, index) => (
-              <FeedDetail
-                key={index}
-                name={item.name}
-                image={unknownUser as string}
-                title={item.title}
-                desc={'description'}
-              ></FeedDetail>
-            ))}
-          </InfiniteScroll>
-        </div>
+        <InfiniteScroll
+          dataLength={items.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          endMessage={<h4>모든 데이터 로드 완료!</h4>}
+          loader={<h4>Loading...</h4>}
+          scrollableTarget="main-styled"
+        >
+          {items.map((item, index) => (
+            <FeedDetail
+              key={index}
+              name={item.name}
+              image={unknownUser as string}
+              title={item.title}
+              desc={'description'}
+            ></FeedDetail>
+          ))}
+        </InfiniteScroll>
       </FeedListContainer>
     </Main>
   );
