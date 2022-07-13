a6480eedb5c42b6c8c321c64a82676f685ef0218
dnjsqhwo <dnjsqhwo@gmail.com>
2022-07-13 18:29:30 +0900

    chore: 주석 삭제

--- front-team7/src/lib/axios.ts
+++ front-team7/src/lib/axios.ts
@@ -29,42 +29,8 @@ axios.interceptors.response.use(
   (error: AxiosError) => {
     // const message = error.response?.data?.message || error.message;
     // 에러 처리..(toast)
+    console.log(error);
 
     return Promise.reject(error);
   }
 );
-
-// export const useAxiosInterceptor = () => {
-//   const auth = useRecoilValue(authAtom);
-
-//   useEffect(() => {
-//     const reqInterceptor = axios.interceptors.request.use(
-//       (config: AxiosRequestConfig) => {
-//         // config.headers!['Authorization'] = `Bearer ${auth.token || ''}`;
-//         config.headers!['Accept'] = 'application/json';
-//         config.headers!['Content-Type'] = 'application/json';
-//         return config;
-//       },
-//       (error: AxiosError) => {
-//         return Promise.reject(error);
-//       }
-//     );
-
-//     const resInterceptor = axios.interceptors.response.use(
-//       (response: AxiosResponse) => {
-//         return response.data;
-//       },
-//       (error: AxiosError) => {
-//         // const message = error.response?.data?.message || error.message;
-//         // 에러 처리..(toast)
-
-//         return Promise.reject(error);
-//       }
-//     );
-
-//     return () => {
-//       axios.interceptors.request.eject(reqInterceptor);
-//       axios.interceptors.response.eject(resInterceptor);
-//     };
-//   }, []);
-// };
