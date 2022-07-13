015cd66805bddd1c1ecea9c4a13b1a3638b821f5
dnjsqhwo <dnjsqhwo@gmail.com>
2022-07-13 13:28:15 +0900

    fix: loading state 추가하여 렌더링 문제 해결

--- front-team7/src/features/auth/api/login.ts
+++ front-team7/src/features/auth/api/login.ts
@@ -1,20 +1,25 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { axios } from '@/lib';
 import { useSetRecoilState } from 'recoil';
 
 import { authState, type AuthState } from '@/store';
 
 export const useLogin = () => {
+  const [loading, setLoading] = useState(true);
   const setAuth = useSetRecoilState(authState);
 
   useEffect(() => {
     async function getAccessToken() {
       await axios.get<void>('/api/loginCheck'); // access, refresh 갱신하는 api
       const user = await axios.get<AuthState>('/api/users/user'); // user 데이터 가져오는 api
-
-      setAuth(user);
+      return user;
     }
 
-    getAccessToken().catch((e) => console.log(e));
+    getAccessToken()
+      .then(setAuth)
+      .catch((e) => console.log(e))
+      .finally(() => setLoading(false));
   }, []);
+
+  return { loading };
 };
