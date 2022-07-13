126dcd4b59cd3f99281e1d1cda1e4eead364dff3
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-07-13 22:13:02 +0900

    chore : redis 코드 주석 처리

--- back-team7/src/routes/api/feeds.ts
+++ back-team7/src/routes/api/feeds.ts
@@ -3,7 +3,7 @@ import { feedService } from '../../services';
 import { upload } from '../../middlewares/';
 import { Types } from 'mongoose';
 import { getPostImageList } from '../../utils/img';
-import { redisClient } from '../../server';
+// import { redisClient } from '../../server';
 const feedRouter = Router();
 
 feedRouter.post(
@@ -55,21 +55,21 @@ feedRouter.get('/:_id', async (req: Request, res: Response, next: NextFunction) 
   }
 });
 
-feedRouter.get('/:_id/like', async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const feedId = req.params._id;
-    const userId = req.user!._id;
+// feedRouter.get('/:_id/like', async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     const feedId = req.params._id;
+//     const userId = req.user!._id;
 
-    const newUser = await redisClient.sAdd(`feeds:like:${feedId}`, `${userId}`);
-    if (!newUser) {
-      redisClient.sRem(`feeds:like:${feedId}`, `${userId}`);
-    }
-    const like = await redisClient.sCard(`feeds:like:${feedId}`);
-    res.status(200).json(like);
-  } catch (error) {
-    next(error);
-  }
-});
+//     const newUser = await redisClient.sAdd(`feeds:like:${feedId}`, `${userId}`);
+//     if (!newUser) {
+//       redisClient.sRem(`feeds:like:${feedId}`, `${userId}`);
+//     }
+//     const like = await redisClient.sCard(`feeds:like:${feedId}`);
+//     res.status(200).json(like);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 feedRouter.get('/user/:userId', async (req: Request, res: Response, next: NextFunction) => {
   try {
--- back-team7/src/server.ts
+++ back-team7/src/server.ts
@@ -57,14 +57,14 @@ db.on('error', (error: Error) =>
   console.error(`\nMongoDB 연결에 실패하였습니다...\n ${DB_URL} \n  ${error}`)
 );
 
-export const redisClient = createClient();
+// export const redisClient = createClient();
 
-redisClient.on('ready', (err) => console.log('정상적으로 Redis 서버에 연결되었습니다.'));
-redisClient.on('error', (error: Error) =>
-  console.error(`\nRedis 연결에 실패하였습니다...\n ${DB_URL} \n  ${error}`)
-);
+// redisClient.on('ready', (err) => console.log('정상적으로 Redis 서버에 연결되었습니다.'));
+// redisClient.on('error', (error: Error) =>
+//   console.error(`\nRedis 연결에 실패하였습니다...\n ${DB_URL} \n  ${error}`)
+// );
 
-redisClient.connect();
+// redisClient.connect();
 
 // app.get('/', async (req, res, next) => {
 //   redisClient.sAdd('test', '2');
