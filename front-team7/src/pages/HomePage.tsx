f4dd243246761283bced2b428b444efeab8eeee5
shinbian11 <shinbian11@gmail.com>
2022-07-18 16:03:55 +0900

    fix : 페이지네이션 처리 이후 무한 스크롤 이슈 해결중..

--- front-team7/src/pages/HomePage.tsx
+++ front-team7/src/pages/HomePage.tsx
@@ -8,6 +8,7 @@ import * as UserApi from '@/api/users';
 import { useEffect, useState, CSSProperties } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import Loading from '@/components/Loading/Loading';
+import { axios } from '@/lib';
 
 const StyledFeedListContainer = styled.div`
   width: 100%;
@@ -53,111 +54,85 @@ interface FeedProps {
   createdAt: string;
 }
 
-interface FeedListProps extends Array<FeedProps> { }
-
-// 추후에 feed의 get을 pagination 처리로 몇개씩만 가져올 수 있게끔 구현되면
-// 그떄는 MOCK_ITEMS 없애고, fetchMoreData에서 가져오는 부분 (axios.get) 구현하기
-
-const MOCK_ITEMS: FeedListProps = [
-  { userName: '김지환', title: '👍🏽 홀로 여행기1', description: '설명!' },
-  { userName: '김지환', title: '👍🏽 홀로 여행기2', description: '설명!' },
-  { userName: '김지환', title: '👍🏽 홀로 여행기3', description: '설명!' },
-];
+interface FeedListProps extends Array<FeedProps> {}
 
 const HomePage = () => {
-  // let name = '김지환';
-  // let title = '👍🏽 홀로 여행기';
   const [feedList, setFeedList] = useState<FeedListProps>([]);
   const [hasMore, setHasMore] = useState<boolean>(true);
   const [myUserId, setMyUserId] = useState<string>();
-
-  const fetchMoreData = () => {
-    console.log('feedList.length : ', feedList.length);
-    if (feedList.length >= 10) {
-      setHasMore(false);
-      return;
-    }
-    setTimeout(() => {
-      const newItems = feedList.concat(MOCK_ITEMS);
-      setFeedList(newItems);
-    }, 1000);
-  };
-
-  // const sendData = {
-  //   userName: '김지환',
-  //   title: '신기한 POST의 세계',
-  //   description: '저는 지금 POST를 구현중입니다.',
-  //   address: '서울시 광진구',
-  //   location: {
-  //     lat: 1,
-  //     lng: 2,
-  //   },
-  // };
-
-  // const updateFeedID = '62cbc5aacc2e9840852d11bf';
-  // const updateData = {
-  //   userName: '수정된 김지환',
-  //   title: '수정된 PUT의 세계',
-  //   description: '저는 지금 PUT를 구현중입니다.',
-  //   address: '서울시 수정구 수정동',
-  //   location: {
-  //     lat: 11111,
-  //     lng: 22222,
-  //   },
-  // };
-
-  // delete 구현 (onClick 이벤트)
-  // async function del() {
-  //   if (confirm('삭제하시겠습니까?')) {
-  //     // const deleteFeedID = '62cbc449cc2e9840852d11b1';
-  //     const result: FeedListProps = await Api.deleteOneFeed(deleteFeedID);
-  //     console.log('delete() : ', result);
-  //   }
-  // }
+  const [page, setPage] = useState<number>(1);
+  const [perPage, setPerPage] = useState<number>(3);
+  const [totalPage, setTotalPage] = useState<number>(0);
 
   useEffect(() => {
     async function get() {
-      const result: FeedListProps = await Api.getAllFeeds();
-      setFeedList(result);
+      const result = await axios.get('/api/feeds/page/list', {
+        params: {
+          page: page,
+          perPage: perPage,
+        },
+      });
+      const initialList = result.feedList;
+      const totalPage = result.totalPage;
+
+      setTotalPage(totalPage);
+      setPage((page) => page + 1);
+
+      setFeedList(initialList);
     }
 
     async function getMyUserId() {
       const myInfo = await UserApi.getMyUserInfo();
       setMyUserId(myInfo._id);
-      console.log('myUserId : ', myUserId);
     }
-    // create 구현
-    // async function create() {
-    //   console.log('sendData : ', sendData);
-    //   const result: FeedListProps = await Api.createOneFeeds(sendData);
-    //   console.log('create() : ', result);
-    // }
-    // create();
 
-    // update 구현
-    // async function update() {
-    //   console.log('updateData : ', updateData);
-    //   const result: FeedListProps = await Api.updateOneFeed(updateFeedID, updateData);
-    //   console.log('update() : ', result);
-    // }
-
-    // update();
     get();
     getMyUserId();
   }, []);
+
+  const fetchMoreData = () => {
+    // fetchMoreData 함수가 처음 한번밖에 실행이 안됨...
+
+    // alert('?');
+    // console.log('hasMore : ', hasMore);
+    // console.log('typeof page, perPage, totalPage', typeof page, typeof perPage, typeof totalPage);
+    // console.log('fetchMoreData : page, perPage, totalPage : ', page, perPage, totalPage);
+
+    if (page > totalPage) {
+      setHasMore(false);
+      return;
+    }
+
+    setTimeout(async () => {
+      const newItems = await axios.get('/api/feeds/page/list', {
+        params: {
+          page: page,
+          perPage: perPage,
+        },
+      });
+      setPage((page) => page + 1);
+
+      // console.log('지금까지 존재하는 전체 리스트들 : ', feedList);
+      // console.log('이번에 가져온 리스트들 : ', newItems.feedList);
+      const newFeedList = feedList.concat(newItems.feedList);
+
+      // console.log('합친 리스트들 : ', newFeedList);
+      setFeedList(newFeedList);
+    }, 1000);
+  };
+
   return (
     <Main
-      id="main-styled"
       display={'flex'}
       flexDirection={'column'}
-      // justifyContent={'flex-start'}
       alignItems={'center'}
       padding={'70px 0'}
+      id="main-styled"
     >
       <StyledFeedListContainer>
         <InfiniteScroll
           style={{ overflow: 'visibility' }}
-          dataLength={feedList.length}
+          dataLength={perPage}
           next={fetchMoreData}
           hasMore={hasMore}
           endMessage={<Loading text={'모든 데이터 로드 완료!'}></Loading>}
