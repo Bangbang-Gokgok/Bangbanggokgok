ad2d8570248b0a9558f1f749f7aa14fafe62e86f
dnjsqhwo <dnjsqhwo@gmail.com>
2022-07-19 06:53:36 +0900

    chore: 자잘한 코드 변경

--- front-team7/src/features/user/api/userRequests.ts
+++ front-team7/src/features/user/api/userRequests.ts
@@ -1,7 +1,19 @@
 import { axios } from '@/lib';
 import { UserResponse } from '@/store';
 
+export interface UserByIdDto {
+  email: string;
+  name: string;
+  profileImage?: string[];
+  description?: string;
+}
+
 export async function getCurrentUser() {
   const user = await axios.get<never, UserResponse>('/api/users/user');
+  return user;
+}
+
+export async function getUserById(userId?: string | null) {
+  const user = await axios.get<never, UserByIdDto>(`/api/users/${userId}`);
   return user;
 }
