ea2461e657559d5d41cc430d971f5895af61267f
dnjsqhwo <dnjsqhwo@gmail.com>
2022-07-20 07:03:37 +0900

    chore: 불필요한 코드 수정

--- front-team7/src/features/admin/api/getAllUsers.ts
+++ front-team7/src/features/admin/api/getAllUsers.ts
@@ -3,15 +3,13 @@ import { useState, useEffect } from 'react';
 import { getAllUsers, type AllUsers } from '@/api/users';
 
 export const useGetAllUsers = () => {
-  const [userInfoData, setUserInfoData] = useState<Partial<AllUsers>[] | null>(null);
+  const [userInfoData, setUserInfoData] = useState<Partial<AllUsers>[] | null>([]);
 
   useEffect(() => {
     getAllUsers()
-      .then((res) => {
-        setUserInfoData(res);
-      })
+      .then((res) => setUserInfoData(res))
       .catch((e) => console.log(e));
-  }, []);
+  }, [userInfoData]);
 
   return userInfoData;
 };
--- front-team7/src/features/admin/api/index.ts
+++ front-team7/src/features/admin/api/index.ts
@@ -1,1 +1,2 @@
 export * from './getAllUsers';
+export * from './getAllFeeds';
