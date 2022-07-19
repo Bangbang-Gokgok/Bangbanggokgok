6678fce81847c36235ad04d4ccf6c0b7ee5cbec3
dnjsqhwo <dnjsqhwo@gmail.com>
2022-07-20 07:05:15 +0900

    chore: UserInfo 컴포넌트와 스타일이 동일한 임시 컴포넌트 생성

--- front-team7/src/features/admin/components/UserInfoTemp.tsx
+++ front-team7/src/features/admin/components/UserInfoTemp.tsx
@@ -4,16 +4,19 @@ import { Link } from 'react-router-dom'; // 해당 피드를 작성한 유저로
 import { Avartar } from '@/components/Avatar';
 
 export interface UserInfoProps {
+  id: string;
   name: string;
   image?: string;
 }
 
-export const UserInfo = ({ name, image }: UserInfoProps) => {
+export const UserInfo = ({ id, name, image }: UserInfoProps) => {
   return (
-    <StyledUserInfo>
-      <Avartar kind="circle" size="md" src={image} alt="img1" />
-      <span className="userinfo-username">{name}</span>
-    </StyledUserInfo>
+    <Link style={{ color: '#a3a3a3' }} to={`/profile/${id}`}>
+      <StyledUserInfo>
+        <Avartar kind="circle" size="md" src={image} alt="img1" />
+        <span className="userinfo-username">{name}</span>
+      </StyledUserInfo>
+    </Link>
   );
 };
 
