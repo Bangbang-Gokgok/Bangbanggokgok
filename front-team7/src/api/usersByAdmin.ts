3dcc4891e312460688543d54e94f946a6a3362be
dnjsqhwo <dnjsqhwo@gmail.com>
2022-07-20 07:05:37 +0900

    chore: 관리자용 api 타입 추가

--- front-team7/src/api/usersByAdmin.ts
+++ front-team7/src/api/usersByAdmin.ts
@@ -1,13 +1,13 @@
 import { axios } from '@/lib';
 
 export const updateOneUserByAdmin = async (id, updateData) => {
-  return await axios.put(`/api/users/admin/${id}`, updateData, {
+  return await axios.put(`/api/admins/users/${id}`, updateData, {
     headers: { 'Content-Type': `application/json` },
   });
 };
 
 export const deleteOneUserByAdmin = async (id) => {
-  return await axios.delete(`/api/users/admin/${id}`);
+  return await axios.delete(`/api/admins/users/${id}`);
 };
 
 // export const updateOneUserByAdmin = async (id, updateData) => {
