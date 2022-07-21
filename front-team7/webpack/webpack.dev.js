f59aabba403f426d5781a5e4918939ca2e05a6bc
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-07-21 15:14:54 +0900

    setting : host 세팅

--- front-team7/webpack/webpack.dev.js
+++ front-team7/webpack/webpack.dev.js
@@ -17,6 +17,7 @@ module.exports = {
       },
     },
     hot: true, // Webpack의 Hot Module Replacement 기능을 활성화합니다.
+    host: process.env.HOST,
     open: true, // 서버가 시작된 후 브라우저를 열도록 dev-server에 지시합니다.
     compress: true, // 제공되는 모든 항목에 대해 gzip 압축을 활성화합니다.
     historyApiFallback: true, // HTML5 History API를 사용할 때, index.html 페이지는 404 응답 대신 제공되어야 합니다.
