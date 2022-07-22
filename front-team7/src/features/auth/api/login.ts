508266ea60e0a40507929c9048dde1ea030723f5
task11 <task11@naver.com>
2022-07-22 14:37:46 +0900

    chore: 릴리즈 테스트

--- front-team7/src/features/auth/api/login.ts
+++ front-team7/src/features/auth/api/login.ts
@@ -11,12 +11,13 @@ export const useLogin = () => {
 
   useEffect(() => {
     async function getAccessToken(): Promise<UserState> {
-      await axios.get<never, void>('/api/loginCheck'); // access, refresh 갱신하는 api
+
+      // await axios.get<never, void>('/api/loginCheck'); // access, refresh 갱신하는 api
       const user = await getCurrentUser(); // user 데이터 가져오는 api
 
       console.log(user);
 
-      const newUser: UserState & { _id?: string; updatedAt?: string; refreshToken?: string } = {
+      const newUser: UserState & { _id?: string; updatedAt?: string; refreshToken?: string; } = {
         ...user,
         id: user._id,
       };
